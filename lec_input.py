0f070bde2f1e0d4c1e6e55f473e1b84dedb1e049
rinxqzwen <tereschenkoarina39@gmail.com>
2025-10-09 16:50:59 +0000

    my_first_lab

---
+++ lec_input.py
@@ -0,0 +1,9 @@
+a = input ()
+print (a)
+
+a = input("Введите значение a: ")
+print (a)
+print(type(a))
+
+a = int(input("введите значение а: "))
+print (type(a))
---
+++ task4.py
@@ -0,0 +1,7 @@
+a = [1.5, "Good", "Bad"]
+
+b = [9, "Blue", "Red", 11]
+
+c= a[1] + b[3]
+print(c)
+
