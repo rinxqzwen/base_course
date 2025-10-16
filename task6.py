e55185b2f1e761c6c6b804737d86aa2bfed6efce
rinxqzwen <tereschenkoarina39@gmail.com>
2025-10-16 15:56:51 +0000

    uiyi

--- task6.py
+++ task6.py
@@ -1,1 +1,14 @@
-print (input('Возраст пользователя:'), input('пол пользователя:'), input('имя пользователя:'), input('город пользователя:'), input('класс обучения:') )
+a = input ('Возраст пользователя:')
+b = input('пол пользователя:')
+c = input('имя пользователя:')
+d = input('город пользователя:')
+e = input('класс обучения:') 
+
+inf = []
+inf.append(a)
+inf.append(b)
+inf.append(c)
+inf.append(d)
+inf.append(e)
+print(inf)
+
