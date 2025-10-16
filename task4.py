1be478df897021a3357f3c99f41cb170340159f3
rinxqzwen <tereschenkoarina39@gmail.com>
2025-10-16 15:55:15 +0000

    less_2

--- task4.py
+++ task4.py
@@ -2,6 +2,12 @@ a = [1.5, "Good", "Bad"]
 
 b = [9, "Blue", "Red", 11]
 
-c= a[1] + b[3]
-print(c)
+print(str(a[1]) + (b[2]))
 
+print(a[2] + b[2])
+
+print(a[0] * b[0])
+
+print((a[0])**b[3])
+
+print(a + b)
